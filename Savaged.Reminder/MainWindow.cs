6f7db56e225acf4ad6efe4520479b218c15b6097
savaged <savagedavid@gmail.com>
2019-06-07 17:49:03 +0100

    initial commit

---
+++ Savaged.Reminder/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow(string text) : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        label1.Text = text;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
---
+++ Savaged.Reminder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+namespace Savaged.Reminder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            var text = "supply command line arg (in quotes) to go here";
+
+            if (args != null 
+                && args.Length > 0 
+                && !string.IsNullOrEmpty(args[0]))
+            {
+                text = args[0];
+            }
+
+            MainWindow win = new MainWindow(text);
+            win.Show();
+            Application.Run();
+        }
+    }
+}
